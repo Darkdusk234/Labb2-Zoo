1f223c9a9979b4f193efa958d7c2cbdb114986df
darkdusk234 <johan.svensson.01@hotmail.com>
2024-10-23 15:11:09 +0200

    Created hungry field in Animal class

--- Labb2 Zoo/Animal.cs
+++ Labb2 Zoo/Animal.cs
@@ -6,12 +6,15 @@
         public int Age { get; set; }
         public bool Wild { get; set; }
         public bool Sick { get; set; }
-
         public string Handler { get; set; }
+        private bool _hungry;
 
         public virtual void Feed()
         {
+            _hungry = false;
             Console.WriteLine("Djuret fick mat.");
         }
+
+
     }
 }
