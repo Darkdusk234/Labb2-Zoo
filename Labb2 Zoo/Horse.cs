147aea76439be0263ec2c60be7448dfbfe4a5606
darkdusk234 <johan.svensson.01@hotmail.com>
2024-10-25 10:35:59 +0200

    Created empty Constructor in Horse class

--- Labb2 Zoo/Horse.cs
+++ Labb2 Zoo/Horse.cs
@@ -2,6 +2,19 @@
 {
     internal class Horse : Animal
     {
+        public bool ContestWinner { get; set; }
 
+        public Horse()
+        {
+            Name = "Otilldelad";
+            Alive = true;
+            Age = 0;
+            Wild = true;
+            Sick = false;
+            Handler = "Otilldelad";
+            Hungry = false;
+            IsFemale = false;
+            ContestWinner = false;
+        }
     }
 }
