c552bb0e9bc8f644e17e8449fe6902064aa5941e
darkdusk234 <johan.svensson.01@hotmail.com>
2024-10-26 10:40:42 +0200

    Created empty constructor in fox class

--- Labb2 Zoo/Fox.cs
+++ Labb2 Zoo/Fox.cs
@@ -1,7 +1,22 @@
-﻿namespace Labb2_Zoo
-{
-    internal class Fox
-    {
+﻿using System.Xml.Linq;
 
+namespace Labb2_Zoo
+{
+    internal class Fox : Animal
+    {
+        public bool LikesToPlay { get; set; }
+
+        public Fox()
+        {
+            Name = "Otilldelad";
+            Alive = true;
+            Age = 0;
+            Wild = true;
+            Sick = false;
+            Handler = "Otilldelad";
+            Hungry = false;
+            IsFemale = false;
+            LikesToPlay = false;
+        }
     }
 }
