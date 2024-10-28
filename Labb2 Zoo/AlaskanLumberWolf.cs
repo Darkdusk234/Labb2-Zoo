f391c3cd3d24c87674d0c15576ac16c202f2cb65
darkdusk234 <johan.svensson.01@hotmail.com>
2024-10-28 16:27:48 +0100

    Created constructor with input arguments in AlaskanLumberWolf class

--- Labb2 Zoo/AlaskanLumberWolf.cs
+++ Labb2 Zoo/AlaskanLumberWolf.cs
@@ -1,10 +1,16 @@
 ﻿namespace Labb2_Zoo
 {
-    internal class AlaskanLumberWolf
+    internal class AlaskanLumberWolf : Wolf
     {
+        public string BirthPlace { get; set; } = "Otilldelad";
         public AlaskanLumberWolf()
         {
 
+        }
+
+        public AlaskanLumberWolf(string name, int age, bool wild, string handler, bool isFemale, string furColor, string birthPlace) : base(name, age, wild, handler, isFemale, furColor)
+        {
+            BirthPlace = birthPlace;
         }
     }
 }
