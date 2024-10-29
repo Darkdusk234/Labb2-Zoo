9926ae544f224b3cefb034a6853d514b7ce24d12
darkdusk234 <johan.svensson.01@hotmail.com>
2024-10-29 12:40:15 +0100

    Created a instance of some of the different classes in Main

--- Labb2 Zoo/Program.cs
+++ Labb2 Zoo/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Horse horse = new Horse("Hejde", 5, false, "Bert", false, false);
+            Fox fox = new Fox("Zip", 3, false, "Lena", true, true);
+            Direwolf direwolf = new Direwolf("Kraven", 6, true, "Triss", false, "Dark-Gray", 1675);
+            AlaskanLumberWolf lumberwolf = new AlaskanLumberWolf("Brett", 9, false, "Bert", false, "Light-Gray", "Minnesota");
+
+            
         }
     }
 }
